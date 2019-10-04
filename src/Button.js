7940f864ecb46c6353d41dc6817be6892a247cc7
Dan Jackson <danjackson@hastingsdirect.com>
2019-10-04 09:43:24 +0100

    Tabbing fixes within the Button component

--- src/Button.js
+++ src/Button.js
@@ -3,23 +3,23 @@ import classNames from 'classnames'
 
 const Button = ({ type = 'button', url = '', className = '', theme = '', isFullWidth = false, children }) => {
 
-    const buttonClass = classNames('btn', className, {
-        'btn--primary' : theme === 'primary',
-        'btn--secondary' : theme === 'secondary',
-        'btn--full-width' : isFullWidth
-    })
+	const buttonClass = classNames('btn', className, {
+		'btn--primary' : theme === 'primary',
+		'btn--secondary' : theme === 'secondary',
+		'btn--full-width' : isFullWidth
+	})
 
 	const isAnchor = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'))
 
-    const renderAnchor = () =>
-        <a href={url} className={buttonClass}>{children}</a>
+	const renderAnchor = () =>
+		<a href={url} className={buttonClass}>{children}</a>
 
-    const renderButton = () => 
-        <button type={type} className={buttonClass}>{children}</button>
+	const renderButton = () => 
+		<button type={type} className={buttonClass}>{children}</button>
 
-    return (
-        isAnchor ? renderAnchor() : renderButton()
-    )
+	return (
+		isAnchor ? renderAnchor() : renderButton()
+	)
 }
 
 export default Button
