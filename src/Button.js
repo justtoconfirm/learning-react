37ac3fa64ff1065552073e8ec576f013b0e95929
Dan Jackson <danjackson@hastingsdirect.com>
2019-10-04 09:08:08 +0100

    url prop added to the Button component

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({ type = 'button', children }) => {
+const Button = ({ type = 'button', url = '', children }) => {
 
 	const isAnchor = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'))
-	
+
     const renderAnchor = () =>
         <a href={url}>{children}</a>
 
