8cd5d742234047051442cc962cc7618e33182e1b
Dan Jackson <danjackson@hastingsdirect.com>
2019-10-04 09:07:22 +0100

    Render functions added to render the Button component as a link if a value is passed into the url prop

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Button = ({ type = 'button', children }) => {
-	return (
-		<button type={type}>{children}</button>
-	)
+
+	const isAnchor = url && (url.includes('http') || url.startsWith('#') || url.startsWith('mailto') || url.startsWith('/'))
+	
+    const renderAnchor = () =>
+        <a href={url}>{children}</a>
+
+    const renderButton = () => 
+        <button type={type}>{children}</button>
+
+    return (
+        isAnchor ? renderAnchor() : renderButton()
+    )
 }
 
 export default Button
